53fc310a26a724279b77e408b56ae5d192fc8abd
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-01 15:20:03 -0700

    Python

--- python/Python_OOP/BankAccount.py
+++ python/Python_OOP/BankAccount.py
@@ -4,7 +4,7 @@ class BankAccount:
         self.password = password
         self.balance = balance
         self.int_rate = int_rate
-        self.yield_interest()
+        # self.yield_interest()
 
     def deposit(self, amount):
         self.balance += amount
@@ -27,7 +27,7 @@ class BankAccount:
         return self
 
 
-dennis = BankAccount("Dennis Lloyd", "DennisPassword", 4, 100)
-maksim = BankAccount("Maksim Dauhaleu", "MaksimPassword", 3, 400)
-maksim.deposit(100).deposit(200).deposit(200).withdraw(700).yield_interest().Display_Account_Info()
+maksim = BankAccount("Maksim Dauhaleu", "MaksimPassword", 2, 500)
+dennis = BankAccount("Dennis Lloyd", "DennisPassword", 2, 100)
+maksim.deposit(100).yield_interest().Display_Account_Info()
 dennis.deposit(1000).deposit(500).withdraw(300).withdraw(300).withdraw(300).withdraw(300).yield_interest().Display_Account_Info()
