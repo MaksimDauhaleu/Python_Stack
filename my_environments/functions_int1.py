c66bcebc816691b14e6692cef4e45e8c77ab288b
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-09-30 17:05:25 -0700

    Functions 2

---
+++ my_environments/functions_int1.py
@@ -0,0 +1,36 @@
+import random
+def randInt(min=0 , max= 100  ):
+    num = random.random() * 100
+    return num
+
+
+print(randInt())
+
+
+import random
+def randInt(min=0 , max= 100  ):
+    num = random.random() * 50
+    return num
+
+
+print(randInt())
+
+
+
+import random
+def randInt(min=0 , max= 100  ):
+    num = random.random() * 50 + 50
+    return num
+
+
+print(randInt())
+
+
+
+import random
+def randInt(min=0 , max= 100  ):
+    num = random.random() * 450 + 50
+    return num
+
+
+print(randInt())
