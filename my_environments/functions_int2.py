020f7fe618884cf1b7c4ef0f32fc18af6a691725
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-09-30 20:03:23 -0700

    f2

--- my_environments/functions_int2.py
+++ my_environments/functions_int2.py
@@ -16,3 +16,16 @@ z[0].update({'y': 30})
 
 
 
+def iterateDictionary():
+    students = [
+             {'first_name':  'Michael', 'last_name': 'Jordan'},
+             {'first_name': 'John', 'last_name': 'Rosales'},
+             {'first_name': 'Mark', 'last_name': 'Guillen'},
+             {'first_name': 'KB', 'last_name': 'Tonel'}
+        ]
+    print('firs_name -', (students[0]['first_name']), ',', 'last_name -', (students[0]['last_name']))
+    print('firs_name -', (students[1]['first_name']), ',', 'last_name -', (students[1]['last_name']))
+    print('firs_name -', (students[2]['first_name']), ',', 'last_name -', (students[2]['last_name']))
+    print('firs_name -', (students[3]['first_name']), ',', 'last_name -', (students[3]['last_name']))
+iterateDictionary()
+
