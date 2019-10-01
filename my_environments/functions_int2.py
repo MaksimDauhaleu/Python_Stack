edbc664a84c625dbafc1546573230d65f3f74675
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-09-30 18:25:54 -0700

    functions_int2

---
+++ my_environments/functions_int2.py
@@ -0,0 +1,18 @@
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name': 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+x[1][0] = 15
+students[0].update({'last_name': 'Bryant'})
+sports_directory.update({'soccer':['Andres', 'Ronaldo', 'Rooney']})
+z[0].update({'y': 30})
+
+
+
+
