a179f5d86d1b26aaeebcbc5ab6972817a99f1385
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-04 21:59:49 -0700

    dd

--- flask/flask_fundamentals/GuessingGame/server.py
+++ flask/flask_fundamentals/GuessingGame/server.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, session, redirect
+
+import random
+
 app = Flask(__name__)
-app.secret_key= "maks99080403"
+app.secret_key= "maks"
 
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def main():
-    number = request.form['number']
-    
+    if "number"not in session:
+        session['number'] = random.randint(1,100)
+    return render_template("game.html")
 
-    return render_template("game.html", number=number)
-
+@app.route('/getNumb', methods=['POST'])         
+def getNumb():
+    print("*"*80)
+    print(session["number"])
+    print("*"*80)
+    print(request.form)
+    session["guess"]=int(request.form["guess"])
+    return redirect("game.html")
 
 
 
