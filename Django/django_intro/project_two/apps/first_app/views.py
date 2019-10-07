91d7e1b5bd0c84c226635dd63f0a102102f93cc1
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-07 16:13:22 -0700

    f

--- Django/django_intro/project_two/apps/first_app/views.py
+++ Django/django_intro/project_two/apps/first_app/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import redirect # add redirect to import statement
+from django.shortcuts import render, HttpResponse, redirect
 
-def first(request):
-	return redirect("/") 
+def index(request):
+    return HttpResponse("laceholder to later display a list of all blogs")
 
-def one_method(request):                # no values passed via URL
-    pass                                
-    
-def another_method(request, my_val):	# my_val would be a number from the URL
-    pass                                # given the example above, my_val would be 23
-    
-def yet_another(request, name):	        # name would be a string from the URL
-    pass                                # given the example above, name would be 'pooh'
-    
-def one_more(request, id, color): 	# id would be a number, and color a string from the URL
-    pass  
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse("placeholder to display blog number:" + number)
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect('/')
