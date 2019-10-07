50d657d43e5196d17356f90f2fe8f8f060f06e35
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-07 14:38:59 -0700

    g

--- flask/flask_fundamentals/GuessingGame/server.py
+++ flask/flask_fundamentals/GuessingGame/server.py
@@ -1,28 +1,30 @@
-from flask import Flask, render_template, request, session, redirect
-
-import random
-
+from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
-app.secret_key= "maks"
-
-
+app.secret_key = 'candycane'
 @app.route('/')
-def main():
-    if "number"not in session:
-        session['number'] = random.randint(1,100)
-    return render_template("game.html")
-
-@app.route('/getNumb', methods=['POST'])         
-def getNumb():
-    print("*"*80)
-    print(session["number"])
-    print("*"*80)
-    print(request.form)
-    session["guess"]=int(request.form["guess"])
-    return redirect("game.html")
+def index():
+    import random
+    if 'num' not in session:
+        session['num'] = random.randint(1, 100) 
+    print(session['num'])
+    return render_template('game.html')
+@app.route('/guess', methods =['POST'])
+def make_guess():
+    userguess = request.form['guess']
+    if int(userguess) == int(session['num']):
+        return render_template('success.html')
+    if int(userguess) < int(session['num']):
+        return render_template('too_low.html')
+    else:
+        return render_template('too_high.html')
+@app.route('/reset', methods=['POST'])
+def reset_user():
+    session.clear()
+    return redirect('/')
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
    
