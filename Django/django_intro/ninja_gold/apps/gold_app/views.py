3336ffd9dff3fc5023ac37e324b42922160d9259
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-08 15:01:11 -0700

    test

--- Django/django_intro/ninja_gold/apps/gold_app/views.py
+++ Django/django_intro/ninja_gold/apps/gold_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def gold(request):
+    return render(request, 'gold_app/gold.html')
+
+
