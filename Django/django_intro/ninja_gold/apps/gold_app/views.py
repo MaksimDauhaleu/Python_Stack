24f45f467944244c581597a7a4efa9a41f7322f6
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-10-08 17:17:53 -0700

    d

--- Django/django_intro/ninja_gold/apps/gold_app/views.py
+++ Django/django_intro/ninja_gold/apps/gold_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 
+
 def gold(request):
+    if "gold" not in request.session:
+        request.session['gold'] = 0
     return render(request, 'gold_app/gold.html')
 
+# def process_farm(request):
 
-def 
